0040b13dd8160379c516c498c67959065c767a1c
Lakshmi Prasad Sirigiri <Lakshmi_Sirigiri@contractor.amat.com>
2018-01-03 14:32:37 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
+import { DataService } from './data.service';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
@@ -22,7 +22,7 @@ import { AboutComponent } from './about/about.component';
     AppRoutingModule, BrowserAnimationsModule,
     FormsModule 
   ],
-  providers: [],
+  providers: [DataService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
